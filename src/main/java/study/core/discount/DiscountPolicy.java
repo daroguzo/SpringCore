deb2769cd932b8c93a5f2f62e9c69524ebeadb65
daroguzo <kjw2122@naver.com>
2021-09-14 20:03:34 +0900

    create Order, DiscountPolicy

---
+++ src/main/java/study/core/discount/DiscountPolicy.java
@@ -0,0 +1,14 @@
+package study.core.discount;
+
+import study.core.member.Member;
+
+public interface DiscountPolicy {
+
+    /**
+     *
+     * @param member
+     * @param price
+     * @return 할인 대상 금액
+     */
+    int discount(Member member, int price);
+}
