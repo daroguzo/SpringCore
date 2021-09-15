f44dabf753be14162fc48d105994d87707f427cf
daroguzo <kjw2122@naver.com>
2021-09-15 15:21:28 +0900

    create new discount policy, test

---
+++ src/test/java/study/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,39 @@
+package study.core.discount;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import study.core.member.Grade;
+import study.core.member.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RateDiscountPolicyTest {
+
+    RateDiscountPolicy discountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("VIP 10% discount success")
+    public void vip_discount() throws Exception {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.VIP);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(1000);
+    }
+
+    @Test
+    @DisplayName("basic 10% discount")
+    public void basic_discount() throws Exception {
+        // given
+        Member member = new Member(1L, "memberVIP", Grade.BASIC);
+
+        // when
+        int discount = discountPolicy.discount(member, 10000);
+
+        // then
+        assertThat(discount).isEqualTo(0);
+    }
+}
