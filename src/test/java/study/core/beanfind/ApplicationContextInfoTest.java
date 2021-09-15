365efe5980a20e276f0b0be450ee98ad53e1e925
daroguzo <kjw2122@naver.com>
2021-09-15 21:36:51 +0900

    컨테이너에 등록된 모든 빈 조회

---
+++ src/test/java/study/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,36 @@
+package study.core.beanfind;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import study.core.AppConfig;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("print all beans")
+    public void findAllBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + "\n object = " + bean);
+        }
+    }
+
+    @Test
+    @DisplayName("print application beans")
+    public void findApplicationBean() {
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + "\n object = " + bean);
+            }
+        }
+    }
+}
